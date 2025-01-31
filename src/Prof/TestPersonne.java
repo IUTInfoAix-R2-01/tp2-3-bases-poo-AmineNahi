4ce47399094534505e87b4b1f2b5a1e6fcace9aa
n24004659 <amine.nahi@etu.univ-amu.fr>
2025-01-31 17:37:36 +0100

    Code plus lisible

--- src/Prof/TestPersonne.java
+++ src/Prof/TestPersonne.java
@@ -6,8 +6,10 @@ public class TestPersonne{
 		   Personne p2 = new Personne ("Lolo");
 		   System.out.println(p1.toString());
 		   System.out.println(p2.toString());
+		   
 		   System.out.println(p1.getNom());
 		   System.out.println(p1.getPrenom());
+		   
 		   System.out.println(p2.getPrenom());
 		   System.out.println(p2.getNom());
 	   }
