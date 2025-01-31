717204f7245658e19f7bcde27e2930f93c6ef457
n24004659 <amine.nahi@etu.univ-amu.fr>
2025-01-31 16:23:13 +0100

    Ajustement de l'exercice 2 et ajout de l'exercice 3 terminé

--- src/Exercice2/Circle.java
+++ src/Exercice2/Circle.java
@@ -3,27 +3,38 @@ package Exercice2;
 public class Circle {
 	 private double radius;
 	 private String color;
+	 
+	 	public Circle() {  
+	 		radius = 1.0;
+	 		color = "red";
+	   }
 	   
-	   // Constructors (overloaded)
-	   /** Constructs a Circle instance with default value for radius and color */
-	 public Circle() {  // 1st (default) constructor
-	      radius = 1.0;
+	 	public Circle(double radius) {  
+	      this.radius = radius;
 	      color = "red";
 	   }
 	   
-	   /** Constructs a Circle instance with the given radius and default color */
-	 public Circle(double r) {  // 2nd constructor
-	      radius = r;
-	      color = "red";
+	   public double getRadius() {
+	 	    return radius; 
 	   }
 	   
-	   /** Returns the radius */
-	 public double getRadius() {
-	    return radius; 
-	   }
-	   
-	   /** Returns the area of this Circle instance */
 	   public double getArea() {
-	      return radius*radius*Math.PI;
+		   	return radius*radius*Math.PI;
 	   }
+	   
+	   public String getColor() { 
+		   return color;
+	   }
+	   
+	   public void setRadius(double newRadius) {
+		   this.radius = newRadius;
+		}
+	   
+	   public void setColor(String newColor) { 
+	   		this.color = newColor;
+	   		}
+	   
+	   public String toString() {
+		   return "Circle[radius=" + this.radius + " color=" + this.color + "]";
+		}
 	}
---
+++ src/Exercice3/Rectangle.java
@@ -0,0 +1,44 @@
+package Exercice3;
+
+public class Rectangle {
+	private float length;
+	private float widht;
+	
+	public Rectangle () {
+		this.length = 1.0f;
+		this.widht = 1.0f;
+	}
+	
+	public Rectangle (float length, float width) {
+		this.length = length;
+		this.widht = width;
+	}
+	
+	public float getLength() {
+		return length;
+	}
+	
+	public float getWidth() {
+		return widht;
+	}
+	
+	public void setLength (float newLength) {
+		this.length = newLength;
+	}
+	
+	public void setWidth (float newWidht) {
+		this.widht = newWidht;
+	}
+	
+	public float getArea () {
+		return this.length * this.widht;
+	}
+	
+	public float getPerimeter () {
+		return (this.length + this.widht) * 2;
+	}
+	
+	public String toString() {
+		   return "Rectangle[length=" + this.length + " width=" + this.widht + "]";
+		}
+}
